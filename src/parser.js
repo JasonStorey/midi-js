7e269b5c3c8b4dc75876c77b836a274c22d3f449
Jason Storey <jason.andrew.storey@gmail.com>
2014-06-29 21:14:33 +0100

    Added parse method with midi buffer validation. No ArrayBuffer.slice in PhantomJS so Chrome only unit tests :(

--- src/parser.js
+++ src/parser.js
@@ -1,5 +1,20 @@
 var Parser = function Parser() {
-
 };
+
+Parser.prototype.parse = function parse(arrayBuffer) {
+	this.validateArrayBuffer(arrayBuffer);
+
+	this.arrayBuffer = arrayBuffer;
+};
+
+Parser.prototype.validateArrayBuffer = function validateArrayBuffer(arrayBuffer) {
+	if(!arrayBuffer.byteLength) {
+		throw new Error('not a valid array buffer');
+	}
+	var midiChunkId = new Uint16Array(arrayBuffer.slice(0, 8));
+	if(String.fromCharCode.apply(null, new Uint16Array(midiChunkId)) !== 'MThd') {
+		throw new Error('invalid midi data');
+	}
+}
 
 module.exports = Parser;
--- test/parser-spec.js
+++ test/parser-spec.js
@@ -1,8 +1,46 @@
 var Parser = require('../src/parser.js');
 
-describe('Parser', function() {	
+describe('Parser', function() {
+	var validMidiData,
+		invalidMidiData;
+
+	beforeEach(function(){
+		validMidiData = getBuffer('MThd');
+		invalidMidiData = getBuffer('invalid data');
+	});
+
 	it('constructor should return an instance of Parser', function() {
 		var parser = new Parser();
 		expect(parser).to.be.an.instanceof(Parser);
 	});
+
+	it('should throw error if array buffer is invalid', function() {
+		var parser = new Parser(),
+			arrayBuffer = ['not an array buffer'];
+
+		expect(function(){ parser.parse(arrayBuffer) }).to.throw('not a valid array buffer');
+	});
+
+	it('should throw error if array buffer does not contain MIDI data', function() {
+		var parser = new Parser();
+		expect(function(){ parser.parse(invalidMidiData) }).to.throw('invalid midi data');
+	});
+
+	it('should cache valid midi buffer', function() {
+		var parser = new Parser();
+		
+		expect(parser.arrayBuffer).to.be.undefined;
+		parser.parse(validMidiData);
+		expect(parser.arrayBuffer).to.equal(validMidiData);
+	});
 });
+
+function getBuffer(str) {
+	var buf = new ArrayBuffer(str.length * 2),
+		bufView = new Uint16Array(buf);
+
+	for (var i = 0, strLen = str.length; i < strLen; i++) {
+		bufView[i] = str.charCodeAt(i);
+	}
+	return buf;
+}
