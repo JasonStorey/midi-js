837e482d24d41f42445a26aefe4bc0047f1aaca7
Jason Storey <jason.andrew.storey@gmail.com>
2014-07-09 15:32:47 +0100

    Removed an implicit global from unit test and had a tidy.

--- test/parser-spec.js
+++ test/parser-spec.js
@@ -2,35 +2,32 @@ var Parser = require('../src/parser.js'),
 	testBuffers = require('./resources/test-buffers.js');
 
 describe('Parser', function() {
-	var typeZeroMidiBufferWithTicks,
+	var parser,
+		typeZeroMidiBufferWithTicks,
+		typeZeroMidiBufferWithFrames,
 		invalidMidiBuffer;
 
 	beforeEach(function() {
+		parser = new Parser();
 		typeZeroMidiBufferWithTicks  = testBuffers.createMidiBuffer({type:0, ticks: true});
 		typeZeroMidiBufferWithFrames = testBuffers.createMidiBuffer({type:0, frames: true});
 		invalidMidiBuffer = new Uint8Array([88,85,104,100]).buffer;
 	});
 
 	it('constructor should return an instance of Parser', function() {
-		var parser = new Parser();
 		expect(parser).to.be.an.instanceof(Parser);
 	});
 
 	it('should throw error if array buffer is invalid', function() {
-		var parser = new Parser(),
-			arrayBuffer = ['not an array buffer'];
-
+		var arrayBuffer = ['not an array buffer'];
 		expect(function(){ parser.parse(arrayBuffer) }).to.throw('not a valid array buffer');
 	});
 
 	it('should throw error if array buffer does not contain MIDI data', function() {
-		var parser = new Parser();
 		expect(function(){ parser.parse(invalidMidiBuffer) }).to.throw('invalid midi data');
 	});
 
-	it('should cache valid midi buffer', function() {
-		var parser = new Parser();
-		
+	it('should cache valid midi buffer', function() {				
 		expect(parser.arrayBuffer).to.be.undefined;
 		parser.parse(typeZeroMidiBufferWithTicks);
 		expect(parser.arrayBuffer).to.equal(typeZeroMidiBufferWithTicks);
@@ -38,8 +35,7 @@ describe('Parser', function() {
 
 	describe('returns a midi object with a header', function() {
 		it('containing a chunkID', function() {
-			var parser = new Parser(),
-				expectedChunkID = 'MThd',
+			var expectedChunkID = 'MThd',
 				midiObject;
 			
 			midiObject = parser.parse(typeZeroMidiBufferWithTicks);
@@ -47,8 +43,7 @@ describe('Parser', function() {
 		});
 
 		it('containing a chunkSize', function() {
-			var parser = new Parser(),
-				expectedChunkSize = 6,
+			var expectedChunkSize = 6,
 				midiObject;
 			
 			midiObject = parser.parse(typeZeroMidiBufferWithTicks);
@@ -56,8 +51,7 @@ describe('Parser', function() {
 		});
 
 		it('containing a formatType', function() {
-			var parser = new Parser(),
-				expectedFormatType = 0,
+			var expectedFormatType = 0,
 				midiObject;
 			
 			midiObject = parser.parse(typeZeroMidiBufferWithTicks);
@@ -65,8 +59,7 @@ describe('Parser', function() {
 		});
 
 		it('containing numberOfTracks', function() {
-			var parser = new Parser(),
-				expectedNumberOfTracks = 1,
+			var expectedNumberOfTracks = 1,
 				midiObject;
 			
 			midiObject = parser.parse(typeZeroMidiBufferWithTicks);
@@ -74,8 +67,7 @@ describe('Parser', function() {
 		});
 
 		it('containing timeDivision with ticksPerBeat', function() {
-			var parser = new Parser(),
-				expectedTimeDivision = {
+			var expectedTimeDivision = {
 					type: 0,
 					ticksPerBeat: 120
 				},
@@ -86,8 +78,7 @@ describe('Parser', function() {
 		});
 
 		it('containing timeDivision with framesPerSecond and ticksPerFrame', function() {
-			var parser = new Parser(),
-				expectedTimeDivision = {
+			var expectedTimeDivision = {
 					type: 1,
 					framesPerSecond: 24,
 					ticksPerFrame: 120
@@ -101,8 +92,7 @@ describe('Parser', function() {
 
 	describe('returns a midi object with track chunk', function() {
 		it('containing a chunkID', function() {
-			var parser = new Parser(),
-				expectedChunkID = 'MTrk',
+			var expectedChunkID = 'MTrk',
 				midiObject;
 
 			midiObject = parser.parse(typeZeroMidiBufferWithTicks);
@@ -110,8 +100,7 @@ describe('Parser', function() {
 		});
 
 		it('containing a chunkSize', function() {
-			var parser = new Parser(),
-				expectedChunkSize = 4,
+			var expectedChunkSize = 4,
 				midiObject;
 
 			midiObject = parser.parse(typeZeroMidiBufferWithTicks);
@@ -119,8 +108,7 @@ describe('Parser', function() {
 		});
 
 		it('containing chunk data', function() {
-			var parser = new Parser(),
-				expectedChunkData = new Uint8Array([0x01,0x03,0x03,0x07]),
+			var expectedChunkData = new Uint8Array([0x01,0x03,0x03,0x07]),
 				midiObject;
 
 			midiObject = parser.parse(typeZeroMidiBufferWithTicks);
